40268aab72278ed34b7440951ef8fb54e26330fc
Ivan Čabraja <ivancabraja0@gmail.com>
2022-11-02 19:24:18 +0100

    dodan vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
+  <v-app id="app">
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">Abouttttttttttttttt</router-link>
+      <router-link to="/about">About</router-link>
     </nav>
     <router-view />
-  </div>
+  </v-app>
 </template>
 
 <style lang="scss">
