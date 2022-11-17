e18c4ff71a1f534c2aa4c9a0322ebeff1520d583
Ivan Čabraja <ivancabraja0@gmail.com>
2022-11-17 20:39:00 +0100

    bootstrap & design...

---
+++ src/views/Groups.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="groups">
+    <div class="container-fluid my-4 py-4 col">
+      <h2 class="h2">Search Groups</h2>
+      <div class="border-top border-secondary w-50 mx-auto my-3"></div>
+      <div
+        class="container-fluid my-4 py-4 col-lg-12 col-sm-3 col-md-12 d-flex aligns-items-center justify-content-center"
+      >
+        <input type="text" class="form-control w-50" id="search" />
+      </div>
+    </div>
+    <div class="container md-5 my-4 py-4">
+      <div class="table-responsive">
+        <table
+          class="table m-0 pd-4 py-4 md-4 mx-auto table-striped table-hover shadowbox mx-auto"
+        >
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Larry</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Groups",
+};
+</script>
+
+<style></style>
---
+++ src/views/Profile.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="profile">
+    <div class="container-fluid">
+      <div class="mt-5 py-4 mb-4">
+        <h2>YOUR PROFILE</h2>
+        <div class="border-top border-secondary w-50 mx-auto my-3"></div>
+        <div class="row">
+          <div class="py-4 my-4 col-lg-6 col-md-6 col-sm-3">
+            <h2>test 1</h2>
+            <img class="h-50" src="@/assets/default_test.jpg" />
+          </div>
+
+          <div class="py-4 my-4 col-lg-6 col-md-6 col-sm-3">
+            <h2>test 2</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "profile",
+};
+</script>
+
+<style></style>
