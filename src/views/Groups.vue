9ddc2377c1e7c143fba4c47f23e90b8d7ed7e13f
Ivan Čabraja <ivancabraja0@gmail.com>
2022-12-02 18:14:42 +0100

    uredjen groups.vue dodan dizajn od bootstrapa i funkcije za v-if

--- src/views/Groups.vue
+++ src/views/Groups.vue
@@ -6,7 +6,15 @@
       <div
         class="container-fluid my-4 py-4 col-lg-12 col-sm-3 col-md-12 d-flex aligns-items-center justify-content-center"
       >
-        <input type="text" class="form-control w-50" id="search" />
+        <input
+          type="text"
+          placeholder="Search Groups"
+          class="form-control w-50"
+          id="search"
+        />
+        <button type="button rightbtn " class="btn btn-secondary">
+          <i class="fa-solid fa-search"></i>
+        </button>
       </div>
     </div>
     <div class="container md-5 my-4 py-4">
@@ -14,7 +22,7 @@
         <table
           class="table m-0 pd-4 py-4 md-4 mx-auto table-striped table-hover shadowbox mx-auto"
         >
-          <thead class="thead-dark">
+          <thead class="thead-darkbg">
             <tr>
               <th scope="col">Group Names</th>
             </tr>
@@ -37,25 +45,124 @@
       <h2 class="h2">Create your own group</h2>
       <div class="border-top border-secondary w-50 mx-auto my-3"></div>
     </div>
-    <div class="container md-5 my-4 py-4">
+    <div class="container mt-5 my-4 py-4">
       <div class="table-responsive">
         <table
           class="table m-0 pd-4 py-4 md-4 mx-auto table-striped table-hover shadowbox mx-auto"
         >
-          <thead class="thead-dark">
+          <thead class="thead-darkbg">
             <tr>
-              <th scope="col">Group Names</th>
+              <th scope="col ">
+                <div class="btn float-right" v-if="!clicked">
+                  <button
+                    @click="ClickCreate"
+                    type="button rightbtn "
+                    class="btn-secondary btn-bord"
+                  >
+                    <i class="fa-solid fa-plus"></i> Create Group
+                  </button>
+                </div>
+                <div class="btn float-right" v-if="clicked">
+                  <button
+                    @click="CreateGroup"
+                    type="button rightbtn "
+                    class="btn btn-success btn-bord"
+                  >
+                    <i class="fa-solid fa-check"></i> Create
+                  </button>
+
+                  <button
+                    @click="CancelCreateGroup"
+                    type="button rightbtn "
+                    class="btn btn-danger btn-bord"
+                  >
+                    <i class="fa-solid fa-x"></i> Cancel
+                  </button>
+                </div>
+              </th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
+            <tr v-if="!clicked">
+              <th scope="row">
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <p>Click on the Create Group button</p>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+              </th>
             </tr>
-            <tr>
-              <th scope="row">2</th>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
+
+            <tr v-if="clicked">
+              <th scope="row">
+                <div
+                  class="container-fluid py-4 my-4 col-lg-12 col-sm-6 col-md-6"
+                >
+                  <p class="d-flex justify-content-between text-center">
+                    Your group name:
+                  </p>
+                  <input
+                    type="textforgroup"
+                    placeholder="Group Name"
+                    class="form-control w-50"
+                    id="search"
+                  />
+                </div>
+                <div
+                  class="container-fluid py-4 my-4 col-lg-12 col-sm-6 col-md-6"
+                >
+                  <p class="d-flex justify-content-between text-center">
+                    Your company name:
+                  </p>
+                  <input
+                    type="textforcompany"
+                    placeholder="Company name"
+                    class="form-control w-50"
+                    id="search"
+                  />
+                </div>
+                <div
+                  class="container-fluid py-4 my-4 col-lg-12 col-sm-6 col-md-6"
+                >
+                  <p class="d-flex justify-content-between">Join Group type:</p>
+                  <div class="form-check">
+                    <div class="form-check d-flex justify-content-between">
+                      <input
+                        class="form-check-input d-flex justify-content-between"
+                        type="radio"
+                        name="flexRadioDefault"
+                        id="flexRadioDefault2"
+                        checked
+                      />
+                      <label class="form-check-label" for="flexRadioDefault2">
+                        Invite Only
+                      </label>
+                    </div>
+                    <div class="form-check d-flex justify-content-between">
+                      <input
+                        class="form-check-input d-flex justify-content-between"
+                        type="radio"
+                        name="flexRadioDefault"
+                        id="flexRadioDefault1"
+                      />
+                      <label
+                        class="form-check-label d-flex justify-content-between"
+                        for="flexRadioDefault1"
+                      >
+                        Free Join
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </th>
             </tr>
           </tbody>
         </table>
@@ -67,6 +174,20 @@
 <script>
 export default {
   name: "Groups",
+  data() {
+    return {
+      clicked: false,
+    };
+  },
+  methods: {
+    ClickCreate() {
+      this.clicked = true;
+    },
+    CancelCreateGroup() {
+      this.clicked = false;
+    },
+    CreateGroup() {},
+  },
 };
 </script>
 
