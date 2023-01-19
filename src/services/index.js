05bf4210f68a381918dc614f0d4f333f2860e563
Ivan Čabraja <ivancabraja0@gmail.com>
2023-01-19 11:29:06 +0100

    promijenjen link

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import $router from "@/router";
 let Service = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://appprojectmanager.netlify.app/#/",
   timeout: 1000,
 });
 // Service.interceptors.request.use((request) => {
