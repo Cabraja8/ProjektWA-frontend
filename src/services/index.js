42fddb7124e6094b25841709058261ce76b39f80
Ivan Čabraja <ivancabraja0@gmail.com>
2023-01-19 21:00:10 +0100

    promjenjen link od rendera

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import $router from "@/router";
 let Service = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://projectmanager-8yye.onrender.com/",
   timeout: 1000,
 });
 // Service.interceptors.request.use((request) => {
